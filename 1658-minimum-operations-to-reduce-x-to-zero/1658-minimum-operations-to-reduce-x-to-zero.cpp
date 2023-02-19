cd901dc8d1eabb29c53e12fad50c75ae31d6cf22
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-02-19 12:04:13 +0530

    Time: 507 ms (24.66%), Space: 168.3 MB (14.26%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(vector<int>& arr, int x) {
+        unordered_map<int,int> dict;
+        int n = arr.size(), sum = 0;
+        for(int i = n - 1; i >= 0; i--){
+            sum += arr[i];
+            dict[sum] = n - i;
+        }
+        sum = 0;
+        int ans = n + 1;
+        
+        if(dict.find(x) != dict.end())
+            ans = dict[x];
+        
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            
+            if(sum == x)
+                ans = min(ans, i + 1);
+            
+            if(dict.find(x - sum) != dict.end())
+                ans = min(ans, i + 1 + dict[x - sum]);
+        }
+        return ans == n + 1 ? -1 : ans;
+    }
+};
