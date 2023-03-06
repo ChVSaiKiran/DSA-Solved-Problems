baf67ea95c3169c5b772a122edbf4ac4dce44cfb
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-03-06 12:24:15 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.72%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,6 +1,23 @@
-class Solution:
-    def countGoodNumbers(self, n: int) -> int:
-        mod = 1000000007
-        if n % 2:
-            return (pow(5,n//2+1,mod) * pow(4,n//2,mod)) % mod 
-        return (pow(5,n//2,mod) * pow(4,n//2,mod)) % mod 
+long long power(long long x, long long int y)
+    {
+        int p = 1e9 + 7;
+        long long res = 1;
+        x = x % p;
+        if (x == 0) return 0;
+        while (y > 0){
+            if (y & 1)
+                res = (res*x) % p;
+            y = y>>1; // y = y/2
+            x = (x*x) % p;
+        }
+        return res;
+    }
+class Solution {
+public:
+    int countGoodNumbers(long long n) {
+        int p = 1e9 + 7;
+      long long count_of_4s = n/2, count_of_5s = n - count_of_4s;
+      long long ans = ((power(4LL, count_of_4s) % p*power(5LL, count_of_5s) % p) % p);
+        return (int)ans;
+    }
+};
