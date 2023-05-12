5196729a31cb18b4dd1352819cc7af9b051275b8
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-05-12 22:12:22 +0530

    Time: 6 ms (88.46%), Space: 11.8 MB (75.91%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -3,11 +3,9 @@ public:
     vector<int> grayCode(int n) {
         vector<int> ans = {0,1};
         int size = 2, x = 2;
-        n--;
-        while(n--){
-            for(int i = size - 1; i >= 0; i--){
+        while(--n){
+            for(int i = size - 1; i >= 0; i--)
                 ans.push_back(ans[i] | x);
-            }
             size *= 2;
             x *= 2;
         }
