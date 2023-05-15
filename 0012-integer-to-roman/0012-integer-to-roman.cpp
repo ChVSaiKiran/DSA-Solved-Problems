3196bc549eb4dd3a991acd77910d6db7cf9056ef
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-05-15 19:36:19 +0530

    Time: 16 ms (23.97%), Space: 9.3 MB (27.95%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int,string>> arr = {
+            {1000,"M"}, 
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"}
+        };
+        string ans;
+        for(int i = 0; i < 13; i++){
+            while(num >= arr[i].first){
+                ans += arr[i].second;
+                num -= arr[i].first;
+            }
+        }
+        return ans;
+    }
+};
