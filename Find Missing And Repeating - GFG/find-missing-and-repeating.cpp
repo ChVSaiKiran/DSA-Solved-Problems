124a9686d8dfd18501624b8497cd9ec12c108bab
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-06-29 15:53:09 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -6,38 +6,30 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-    int *findTwoElement(int *arr, int n) {
+    vector<int> findTwoElement(vector<int> A, int n) {
         // code here
-        int ans = 0, sum1 = 0, sum2 = 0;
-        for(int i = 0; i < n; i++){
-            ans = ans ^ (i + 1);
-            ans = ans ^ (arr[i]);
-            sum1 += (i + 1);
-            sum2 += arr[i];
-        }
-        int x = 0, y = 0, temp = ans & ~ (ans-1);
-        for(int i = 0; i < n; i++){
-            if((i+1) & temp)
-                x ^= (i + 1);
+        int xr = 0;
+        for(int i:A)
+            xr ^= i;
+        for(int i = 1; i <= n; i++)
+            xr ^= i;
+        int lset = xr & ~(xr - 1), x = 0, y = 0;
+        for(int i = 1; i <= n; i++){
+            if(i & lset)
+                x ^= i;
             else
-                y ^= (i + 1);
-            if(arr[i] & temp)
-                x ^= arr[i];
+                y ^= i;
+        }
+        for(int i:A){
+            if(i & lset)
+                x ^= i;
             else
-                y ^= arr[i];
+                y ^= i;
         }
-        int count = 0;
-        for(int i = 0; i < n; i++)
-            if(arr[i] == x)
-                count++;
-        int *tempp = new int(2);
-        tempp[0] = x;
-        tempp[1] = y;
-        if(count == 0){
-            tempp[0] = y;
-            tempp[1] = x;
-        }
-        return tempp;
+        for(int i:A)
+            if(i == x)
+                return {x,y};
+        return {y,x};
     }
 };
 
@@ -49,7 +41,7 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-        int a[n];
+        vector<int> a(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
         }
