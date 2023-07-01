f8ed1c8abed774853a3e67432aa3c58c08a78ec5
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-07-01 07:35:49 +0530

    Time: 4 ms (41.70%), Space: 5.8 MB (95.61%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        bool f1 = false, f2 = false;
+        bool flag = false;
         long p = n;
         if(p < 0)
-            p *= -1, f2 = true;
-        if(x < 0)
-            x *= -1, f1 = (p % 2 == 1);
+            p *= -1, flag = true;
         double ans = 1;
         while(p){
             if(p & 1)
@@ -14,10 +12,8 @@ public:
             x *= x;
             p >>= 1;
         }
-        if(f2)
+        if(flag)
             ans = 1 / ans;
-        if(f1)
-            ans *= -1;
         return ans;
     }
 };
