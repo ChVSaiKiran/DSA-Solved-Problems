5f863fd8a82da5e2102833b08db41ba3c3e83b22
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-08-12 23:16:17 +0530

    Time: 11 ms (5.74%), Space: 45.2 MB (9.08%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -10,36 +10,36 @@ class Solution {
         boolean vis[][] = new boolean[n][m];
         int delrow[] = {1, 0, -1, 0};
         int delcol[] = {0, 1, 0, -1};
-        for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++){
-            if(board[i][j] == 'O' && !vis[i][j] && !isBound(n, m, i, j)){
-                Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-                ArrayList<Pair<Integer, Integer>> temp = new ArrayList<>();
-                Pair<Integer, Integer> comb = new Pair<>(i,j);
-                q.add(comb);
-                temp.add(comb);
-                boolean flag = true;
-                while(!q.isEmpty()){
-                    int x = q.peek().getKey(), y = q.peek().getValue();
-                    q.remove();
-                    for(int k = 0; k < 4; k++){
-                        int nrow = x + delrow[k];
-                        int ncol = y + delcol[k];
-                        if(isvalid(n,m,nrow,ncol) && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
-                            vis[nrow][ncol] = true;
-                            if(isBound(n, m, nrow, ncol))
-                                flag = false;
-                            comb = new Pair<>(nrow,ncol);
-                            q.add(comb);
+        for(int i = 1; i < n - 1; i++)
+        for(int j = 1; j < m - 1; j++){
+            if(board[i][j] == 'X' || vis[i][j])
+                continue;
+            Queue<Pair<Integer, Integer>> q = new LinkedList<>();
+            ArrayList<Pair<Integer, Integer>> temp = new ArrayList<>();
+            Pair<Integer, Integer> comb = new Pair<>(i,j);
+            q.add(comb);
+            temp.add(comb);
+            boolean flag = true;
+            while(!q.isEmpty()){
+                int x = q.peek().getKey(), y = q.peek().getValue();
+                q.remove();
+                for(int k = 0; k < 4; k++){
+                    int nrow = x + delrow[k];
+                    int ncol = y + delcol[k];
+                    if(isvalid(n,m,nrow,ncol) && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
+                        vis[nrow][ncol] = true;
+                        if(isBound(n, m, nrow, ncol))
+                            flag = false;
+                        comb = new Pair<>(nrow,ncol);
+                        q.add(comb);
+                        if(flag)
                             temp.add(comb);
-                        }
                     }
                 }
-                if(flag){
-                    for(Pair<Integer, Integer> it: temp)
-                        board[it.getKey()][it.getValue()] = 'X';
-                }
             }
+            if(flag)
+                for(Pair<Integer, Integer> it: temp)
+                    board[it.getKey()][it.getValue()] = 'X';
         }
     }
 }
