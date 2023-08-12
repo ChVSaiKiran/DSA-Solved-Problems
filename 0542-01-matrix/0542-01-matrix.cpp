691879a3ae4c1a6384df12cf0d6a4d04b76d9740
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-08-12 15:56:11 +0530

    Time: 63 ms (92.27%), Space: 32.2 MB (67.81%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,38 +2,34 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 1e6));
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
         queue<pair<int,int>> q;
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
                 if(mat[i][j] == 0){
                     q.push({i, j});
-                    ans[i][j] = 0;
+                    vis[i][j] = true;
                 }
+        int delrow[] = {-1, 0, 1, 0};
+        int delcol[] = {0, 1, 0, -1};
+        int dis = 0;
         while(!q.empty()){
-            int sr = q.front().first, sc = q.front().second;
-            visited[sr][sc] = true;
-            q.pop();
-            if(sr + 1 < n){
-                ans[sr + 1][sc] = min(ans[sr][sc] + 1, ans[sr + 1][sc]);
-                if(!visited[sr + 1][sc])
-                    q.push({sr + 1, sc});
-            }
-            if(sc + 1 < m){
-                ans[sr][sc + 1] = min(ans[sr][sc] + 1, ans[sr][sc + 1]);
-                if(!visited[sr][sc + 1])
-                    q.push({sr, sc + 1});
-            }
-            if(sr - 1 >= 0){
-                ans[sr - 1][sc] = min(ans[sr][sc] + 1, ans[sr - 1][sc]);
-                if(!visited[sr - 1][sc])
-                    q.push({sr - 1, sc});
-            }
-            if(sc - 1 >= 0){
-                ans[sr][sc - 1] = min(ans[sr][sc] + 1, ans[sr][sc - 1]);
-                if(!visited[sr][sc - 1])
-                    q.push({sr, sc - 1});
+            dis++;
+            int size = q.size();
+            while(size--){
+                int row = q.front().first, col = q.front().second;
+                q.pop();
+                for(int i = 0; i < 4; i++){
+                    int nrow = row + delrow[i];
+                    int ncol = col + delcol[i];
+                    if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol]){
+                        vis[nrow][ncol] = true;
+                        ans[nrow][ncol] = dis;
+                        q.push({nrow, ncol});
+                    }
+                }
             }
         }
         return ans;
