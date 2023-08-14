26b305c59591706f3b7100191c4470b7c620af4f
CHAKKA VENKATA SAI KIRAN <106020684+Chakka-Venkata-Sai-Kiran@users.noreply.github.com>
2023-08-15 00:07:25 +0530

    Time: 166 ms (33.22%), Space: 45.8 MB (50.77%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -5,7 +5,7 @@ class Solution {
             s.add(i);
         Queue<String> q = new LinkedList<>();
         q.add(beginWord);
-        int cnt = 0;
+        int cnt = 1;
         while(!q.isEmpty()){
             cnt++;
             int n = q.size();
@@ -14,12 +14,11 @@ class Solution {
                 for(int i = 0; i < u.length(); i++)
                     for(char j = 'a'; j <= 'z'; j++){
                         String v = u.substring(0, i) + j + u.substring(i + 1);
-                        // System.out.println(v);
                         if(s.contains(v)){
+                            if(endWord.equals(v))
+                                return cnt;
                             s.remove(v);
                             q.add(v);
-                            if(endWord.equals(v))
-                                return cnt + 1;
                         }
                     }
                 n--;
