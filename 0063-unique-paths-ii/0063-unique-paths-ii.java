d4bb9744fb39670a01b4dc7f78678d6849011cac
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-07-09 18:55:14 +0530

    Time: 0 ms (100%), Space: 41.5 MB (51.27%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -15,21 +15,21 @@ class Solution {
         if(arr[0][0] == 1)
             return 0;
         int n = arr.length, m = arr[0].length;
-        int dp[][] = new int[n][m];
-        for (int[] row : dp) 
-            Arrays.fill(row, -1); 
+        int prev[] = new int[m];
         for(int i = 0; i < n; i++){
+            int curr[] = new int[m];
             for(int j = 0; j < m; j++){
-                if(i == 0 && j == 0 && arr[i][j] == 0){
-                    dp[i][j] = 1;
+                if(i == 0 && j == 0){
+                    curr[j] = 1;
                 } else if(arr[i][j] == 1){
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 } else{
-                    int up = (i < 1) ? 0 : dp[i - 1][j], left = (j < 1) ? 0 : dp[i][j - 1];
-                    dp[i][j] = up + left;
+                    int up = (i < 1) ? 0 : prev[j], left = (j < 1) ? 0 : curr[j - 1];
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[n - 1][m - 1];
+        return prev[m - 1];
     }
 }
