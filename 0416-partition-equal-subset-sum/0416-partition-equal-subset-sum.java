1bea18c0c66dcd7c2e1d1db03ab7aa1f5f3c3b15
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-07-13 21:04:35 +0530

    Time: 96 ms (20.2%), Space: 57.4 MB (17.6%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int fun(int[] nums, int ind, int target, int dp[][]){
+        if(ind == 0){
+            return dp[0][target] = (nums[0] == target ? 1 : 0);
+        }
+        if(dp[ind][target] != -1){
+            return dp[ind][target];
+        }
+        int notTake = fun(nums, ind - 1, target, dp), Take = 0;
+        if(target >= nums[ind]){
+            Take = fun(nums, ind - 1, target - nums[ind], dp);
+        }
+        return dp[ind][target] = notTake | Take;
+    }
+    public boolean canPartition(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        if(sum % 2 == 1){
+            return false;
+        }
+        int target = sum / 2, n = nums.length, dp[][] = new int[n][target + 1];
+        for(int row[] : dp){
+            Arrays.fill(row, -1);
+        }
+        return fun(nums, n - 1, target, dp) == 1;
+    }
+}
