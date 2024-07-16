f61c6cbd3612b6265f0856630815002973040bae
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-07-16 22:47:41 +0530

    Time: 29 ms (48.97%), Space: 81.1 MB (38.36%) - LeetHub

---
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode ancestor;
+    private boolean getCommonAncestor(TreeNode root, int startValue, int destValue){
+        if(root == null){
+            return false;
+        }
+        boolean left = getCommonAncestor(root.left, startValue, destValue);
+        boolean right = getCommonAncestor(root.right, startValue, destValue);
+        if((left && right)){
+            ancestor = root;
+            return false;
+        }
+        else if(left || right){
+            if(root.val == startValue || root.val == destValue){
+                ancestor = root;
+                return false;
+            } else{
+                return true;
+            }
+        }
+        return root.val == startValue || root.val == destValue;
+    }
+    
+    private boolean f(TreeNode root, int val, boolean flag, StringBuilder s) {
+        if (root == null) {
+            return false;
+        } else if (root.val == val) {
+            return true;
+        } else {
+            boolean left = f(root.left, val, flag, s);
+            boolean right = f(root.right, val, flag, s);
+            if (left) {
+                if (flag) {
+                    s.append("U");
+                } else {
+                    s.append("L");
+                }
+                return true;
+            }
+            if (right) {
+                if (flag) {
+                    s.append("U");
+                } else {
+                    s.append("R");
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        getCommonAncestor(root, startValue, destValue);
+        System.out.println(ancestor.val);
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
+        f(ancestor, startValue, true, s1);
+        f(ancestor, destValue, false, s2);
+        // System.out.println(s1.toString() + " " + s2.toString());
+        return s1.toString() + s2.reverse().toString();
+    }
+}
