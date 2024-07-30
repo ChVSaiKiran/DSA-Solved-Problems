d7d59e0ac0e169c1a9458243611d3ce2ddf1d2bb
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-07-30 09:56:20 +0530

    Time: 42 ms (54.22%), Space: 44.7 MB (54.39%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -2,9 +2,9 @@ class Solution {
     private int f(int[][] arr, int ind, int m, int n, int[][][] dp){
         if(ind == 0){
             if(arr[0][0] <= m && arr[0][1] <= n){
-                return dp[ind][m][n] = 1;
+                return dp[0][m][n] = 1;
             } else{
-                return dp[ind][m][n] = 0;
+                return dp[0][m][n] = 0;
             }
         }
 
@@ -21,28 +21,43 @@ class Solution {
     }
 
     public int findMaxForm(String[] strs, int m, int n) {
-        int size = strs.length;
-        int[][] arr = new int[size][2];
-        int[][][] dp = new int[size][m + 1][n + 1];
+        int size = strs.length, arr[][] = new int[size][2];
         for(int j = 0; j < size; j++){
             int cnt0 = 0, cnt1 = 0;
             for(char i : strs[j].toCharArray()){
-                if(i == '0'){
-                    cnt0++;
-                } else{
-                    cnt1++;
-                }
+                if(i == '0')    cnt0++;
+                else            cnt1++;
             }
             arr[j][0] = cnt0;
             arr[j][1] = cnt1;
         }
 
-        for(int[][] x : dp){
-            for(int[] y : x){
-                Arrays.fill(y, -1);
-            } 
+        int[][] prev = new int[m + 1][n + 1];
+
+        for(int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if(arr[0][0] <= i && arr[0][1] <= j){
+                    prev[i][j] = 1;
+                } else{
+                    prev[i][j] = 0;
+                }
+            }
         }
 
-        return f(arr, size - 1, m, n, dp);
+        for(int ind = 1; ind < size; ind++){
+            int[][] curr = new int[m + 1][n + 1];
+            for(int i = 0; i <= m; i++){
+                for(int j = 0; j <= n; j++){
+                    int notTake = 0 + prev[i][j], Take = Integer.MIN_VALUE;
+                    if(i >= arr[ind][0] && j >= arr[ind][1]){
+                        Take = 1 + prev[i - arr[ind][0]][j - arr[ind][1]];
+                    }
+                    curr[i][j] = Math.max(notTake, Take); 
+                }
+            }
+            prev = curr;
+        }
+        
+        return prev[m][n];
     }
 }
