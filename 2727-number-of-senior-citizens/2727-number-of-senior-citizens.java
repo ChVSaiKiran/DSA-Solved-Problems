772382fbad20259a9171bff105365ebb779ca46f
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-01 15:45:00 +0530

    Time: 3 ms (13.06%), Space: 42.9 MB (21.29%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -1,11 +1,5 @@
 class Solution {
     public int countSeniors(String[] details) {
-        int cnt = 0;
-        for(String s : details){
-            if(Integer.valueOf(s.substring(11,13)) > 60){
-                cnt++;
-            }
-        }
-        return cnt;
+        return (int)Arrays.stream(details).filter(s -> Integer.valueOf(s.substring(11,13)) > 60).count();
     }
 }
