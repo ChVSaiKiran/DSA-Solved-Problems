bcc2dded5df0c104c995416f28e73f70a467867e
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-11 21:31:58 +0530

    Time: 7 ms (21.91%), Space: 10.1 MB (88.45%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -2,18 +2,24 @@ class Solution {
 public:
     string numberToWords(int num) {
         string temp[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        string tens[] = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        string tens[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
         if(num < 20){
             return temp[num];
         } else if(num < 100){
-            return tens[num/10] + ((num % 10 != 0) ? " " + numberToWords(num % 10) : "");
+            int q = num / 10, r = num % 10;
+            return tens[q] + ((r != 0) ? " " + numberToWords(r) : "");
         } else if(num < 1e3){
-            return temp[num/100] + " Hundred" + ((num % 100 != 0) ? " " + numberToWords(num % 100) : "");
+            int q = num / 100, r = num % 100;
+            return temp[q] + " Hundred" + ((r != 0) ? " " + numberToWords(r) : "");
         } else if(num < 1e6){
-            return numberToWords(num/1000) + " Thousand" + ((num % 1000 != 0) ? " " + numberToWords(num % 1000) : "");
+            int q = num / 1e3, r = num % (int)1e3;
+            return numberToWords(q) + " Thousand" + (r != 0 ? " " + numberToWords(r) : "");
         } else if(num < 1e9){
-            return numberToWords((int)(num / 1e6)) + " Million" + ((num % ((int)1e6) != 0) ? " " + numberToWords(num % ((int)1e6)) : "");
+            int q = num / 1e6, r = num % (int)1e6;
+            return numberToWords(q) + " Million" + (r != 0 ? " " + numberToWords(r) : "");
+        } else{
+            int q = num / 1e9, r = num % (int)1e9;
+            return numberToWords(q) + " Billion" + (r != 0 ? " " + numberToWords(r) : "");
         }
-        return numberToWords((int)(num / 1e9)) + " Billion" + ((num % ((int)1e9) != 0) ? " " + numberToWords(num % ((int)1e9)) : "");
     }
 };
