254d3bec0cdf515849a48736a94fb4b6dbec6af0
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-11 21:59:47 +0530

    Time: 7 ms (21.91%), Space: 11.1 MB (56.53%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,25 +1,24 @@
 class Solution {
+private:
+    string lessThanTwenty[20] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    string tens[10] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 public:
-    string numberToWords(int num) {
-        string temp[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        string tens[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        if(num < 20){
-            return temp[num];
-        } else if(num < 100){
-            int q = num / 10, r = num % 10;
-            return tens[q] + ((r != 0) ? " " + numberToWords(r) : "");
-        } else if(num < 1e3){
-            int q = num / 100, r = num % 100;
-            return temp[q] + " Hundred" + ((r != 0) ? " " + numberToWords(r) : "");
-        } else if(num < 1e6){
-            int q = num / 1e3, r = num % (int)1e3;
-            return numberToWords(q) + " Thousand" + (r != 0 ? " " + numberToWords(r) : "");
-        } else if(num < 1e9){
-            int q = num / 1e6, r = num % (int)1e6;
-            return numberToWords(q) + " Million" + (r != 0 ? " " + numberToWords(r) : "");
+    string numberToWords(int n) {
+        int q, r = 0;
+        string ans;
+        if(n < 20){
+            ans = lessThanTwenty[n];
+        } else if(n < 100){
+            q = n / 10, r = n % 10, ans =  tens[q];
+        } else if(n < 1e3){
+            q = n / 100, r = n % 100, ans = lessThanTwenty[q] + " Hundred";
+        } else if(n < 1e6){
+            q = n / 1e3, r = n % (int)1e3, ans = numberToWords(q) + " Thousand";
+        } else if(n < 1e9){
+            q = n / 1e6, r = n % (int)1e6, ans = numberToWords(q) + " Million";
         } else{
-            int q = num / 1e9, r = num % (int)1e9;
-            return numberToWords(q) + " Billion" + (r != 0 ? " " + numberToWords(r) : "");
+            q = n / 1e9, r = n % (int)1e9, ans =  numberToWords(q) + " Billion";
         }
+        return (r == 0) ? ans : ans + " " + numberToWords(r);
     }
 };
