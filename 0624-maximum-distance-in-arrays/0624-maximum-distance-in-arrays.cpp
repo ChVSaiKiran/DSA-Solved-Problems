f90df65fc71f7966610bc12c2b0475f82d318cbd
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-16 21:58:38 +0530

    Time: 226 ms (72.17%), Space: 107.8 MB (69.81%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -4,8 +4,7 @@ public:
         int n = arrays.size(), ans = 0, small = arrays[0].front(), big = arrays[0].back();
 
         for(int i = 1; i < n; i++){
-            ans = max(ans, abs(arrays[i].back() - small));
-            ans = max(ans, abs(big - arrays[i].front()));
+            ans = max({ans, abs(arrays[i].back() - small), abs(big - arrays[i].front())});
             big = max(big, arrays[i].back());
             small = min(small, arrays[i].front());
         }
