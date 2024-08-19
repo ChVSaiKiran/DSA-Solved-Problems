1922894912b8d3a4375430e49616bc74c58d9ad8
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-19 15:58:05 +0530

    Time: 22 ms (68.69%), Space: 25.7 MB (69.91%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,19 +1,76 @@
+// // My Code
+// class Solution {
+// private:
+//     int f(string text1, string text2, int ind1, int ind2, vector<vector<int>> &dp){
+//         if(ind1 == 0 && ind2 == 0){
+//             return dp[0][0] = (text1[0] == text2[0]);
+//         }
+
+//         if(dp[ind1][ind2] != -1){
+//             return dp[ind1][ind2];
+//         }
+
+//         if(ind1 == 0 || ind2 == 0){
+//             if(text1[ind1] == text2[ind2]){
+//                 return dp[ind1][ind2] = 1;
+//             }
+//             return dp[ind1][ind2] = f(text1, text2, (ind1 != 0 ? ind1 - 1 : 0), (ind2 != 0 ? ind2 - 1 : 0), dp);
+//         }
+
+//         if(text1[ind1] == text2[ind2]){
+//             return dp[ind1][ind2] = 1 + f(text1, text2, ind1 - 1, ind2 - 1, dp);
+//         }
+
+//         int notPick1 = f(text1, text2, ind1, ind2 - 1, dp);
+//         int notPick2 = f(text1, text2, ind1 - 1, ind2, dp);
+
+//         return dp[ind1][ind2] = max(notPick1, notPick2);
+//     }
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         // Memoization Approach
+//         // int n = text1.length(), m = text2.length();
+//         // vector<vector<int>> dp(n, vector<int>(m, -1));
+//         // return f(text1, text2, n - 1, m - 1, dp);
+
+//         // Tabulation Approach
+//         int n = text1.length(), m = text2.length();
+//         vector<vector<int>> dp(n, vector<int>(m, 0));
+//         dp[0][0] = (text1[0] == text2[0]);
+
+//         for(int ind1 = 1; ind1 < n; ind1++){
+//             dp[ind1][0] = (text1[ind1] == text2[0]) ? 1 : dp[ind1 - 1][0];
+//         }
+
+//         for(int ind2 = 1; ind2 < m; ind2++){
+//             dp[0][ind2] = (text1[0] == text2[ind2]) ? 1 : dp[0][ind2 - 1];
+//         }
+
+//         for(int ind1 = 1; ind1 < n; ind1++){
+//             for(int ind2 = 1; ind2 < m; ind2++){
+//                 if(text1[ind1] == text2[ind2]){
+//                     dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+//                 } else{
+//                     dp[ind1][ind2] = max(dp[ind1][ind2 - 1], dp[ind1 - 1][ind2]);
+//                 }
+//             }
+//         }
+
+//         return dp[n - 1][m - 1];
+//     }
+// };
+
+
+// Better Way of Writing
 class Solution {
 private:
     int f(string text1, string text2, int ind1, int ind2, vector<vector<int>> &dp){
-        if(ind1 == 0 && ind2 == 0){
-            return dp[0][0] = (text1[0] == text2[0]);
+        if(ind1 < 0 || ind2 < 0){
+            return 0;
         }
 
         if(dp[ind1][ind2] != -1){
             return dp[ind1][ind2];
-        }
-
-        if(ind1 == 0 || ind2 == 0){
-            if(text1[ind1] == text2[ind2]){
-                return dp[ind1][ind2] = 1;
-            }
-            return dp[ind1][ind2] = f(text1, text2, (ind1 != 0 ? ind1 - 1 : 0), (ind2 != 0 ? ind2 - 1 : 0), dp);
         }
 
         if(text1[ind1] == text2[ind2]){
@@ -25,36 +82,46 @@ private:
 
         return dp[ind1][ind2] = max(notPick1, notPick2);
     }
+
 public:
     int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length(), m = text2.length();
         // Memoization Approach
-        // int n = text1.length(), m = text2.length();
         // vector<vector<int>> dp(n, vector<int>(m, -1));
         // return f(text1, text2, n - 1, m - 1, dp);
 
-        // Tabulation Approach
-        int n = text1.length(), m = text2.length();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        dp[0][0] = (text1[0] == text2[0]);
+        // Tabulation
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));  //1 Based Indexing
 
-        for(int ind1 = 1; ind1 < n; ind1++){
-            dp[ind1][0] = (text1[ind1] == text2[0]) ? 1 : dp[ind1 - 1][0];
-        }
+        // for(int ind1 = 1; ind1 <= n; ind1++){
+        //     for(int ind2 = 1; ind2 <= m; ind2++){
+        //         if(text1[ind1 - 1] == text2[ind2 - 1]){  
+        //             dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+        //         }
+        //         else{
+        //             dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+        //         }
+        //     }
+        // }
 
-        for(int ind2 = 1; ind2 < m; ind2++){
-            dp[0][ind2] = (text1[0] == text2[ind2]) ? 1 : dp[0][ind2 - 1];
-        }
+        // return dp[n][m];
 
-        for(int ind1 = 1; ind1 < n; ind1++){
-            for(int ind2 = 1; ind2 < m; ind2++){
-                if(text1[ind1] == text2[ind2]){
-                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
-                } else{
-                    dp[ind1][ind2] = max(dp[ind1][ind2 - 1], dp[ind1 - 1][ind2]);
+        // Space Optimization
+        vector<int> prev(m + 1, 0);  //1 Based Indexing
+
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            vector<int> curr(m + 1, 0); 
+            for(int ind2 = 1; ind2 <= m; ind2++){
+                if(text1[ind1 - 1] == text2[ind2 - 1]){  
+                    curr[ind2] = 1 + prev[ind2 - 1];
+                }
+                else{
+                    curr[ind2] = max(prev[ind2], curr[ind2 - 1]);
                 }
             }
+            prev = curr;
         }
 
-        return dp[n - 1][m - 1];
+        return prev[m];
     }
 };
