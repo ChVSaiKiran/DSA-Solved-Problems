058c177cedaad8a5c02b02c2f7b9170ac783359a
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-24 19:21:33 +0530

    Time: 13 ms (15.85%), Space: 47.5 MB (21.05%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,28 @@
+class Solution {
+    private boolean f(int[] weights, int days, int maxCapacity){
+        int currWeight = 0, noOfDays = 0;
+        for(int i : weights){
+            currWeight += i;
+            if(currWeight >= maxCapacity){
+                noOfDays++;
+                currWeight = (currWeight == maxCapacity) ? 0 : i;
+            }
+        }
+        noOfDays += (currWeight != 0 ? 1 : 0);
+        return noOfDays <= days;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low = Arrays.stream(weights).max().getAsInt();
+        int high = (low * weights.length), ans = high;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(f(weights, days, mid)){
+                ans = mid;
+                high = mid - 1;
+            } else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
