2ab0c2952e86153ae79c06966200bce608bc8972
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-30 19:52:07 +0530

    Time: 116 ms (5.04%), Space: 48.5 MB (21.06%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -27,13 +27,42 @@ class Solution {
     }
 
     public boolean isMatch(String s, String p) {
+        // Memoization Approach
+        // int n = s.length(), m = p.length();
+        // int dp[][] = new int[n + 1][m + 1];
+        // for(int row[] : dp){
+        //     Arrays.fill(row, -1);
+        // }
+        // return f(s, p, n - 1, m - 1, dp) == 1;
+
+        // Tabulation Approach
         int n = s.length(), m = p.length();
         int dp[][] = new int[n + 1][m + 1];
-
-        for(int row[] : dp){
-            Arrays.fill(row, -1);
+        dp[0][0] = 1;
+        for(int ind2 = 1; ind2 <= m; ind2++){
+            int i = ind2;
+            while(i > 0 && p.charAt(i - 1) == '*'){
+                i--;
+            }
+            dp[0][ind2] = i == 0 ? 1 : 0;
         }
 
-        return f(s, p, n - 1, m - 1, dp) == 1;
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            for(int ind2 = 1; ind2 <= m; ind2++){
+                if(p.charAt(ind2 - 1) == '?'){
+                    dp[ind1][ind2] = dp[ind1 - 1][ind2 - 1];
+                } else if(p.charAt(ind2 - 1) == '*'){
+                    int flag = 0;
+                    for(int i = ind1; i >= 0; i--){
+                        flag |= dp[i][ind2 - 1];
+                    }
+                    dp[ind1][ind2] = flag;
+                } else{
+                    dp[ind1][ind2] = (s.charAt(ind1 - 1) == p.charAt(ind2 - 1) ? 1 : 0) & dp[ind1 - 1][ind2 - 1];
+                }
+            }
+        }
+
+        return dp[n][m] == 1;
     }
 }
