8a961da5a26039210900a1dd7248cd181d296d18
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-31 07:01:21 +0530

    Time: 19 ms (64.7%), Space: 48.2 MB (32.1%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,10 +1,41 @@
 class Solution {
+    // // 0 Based Indexing
+    // private int f(String s, String p, int ind1, int ind2, int[][] dp){
+    //     if(ind1 < 0 && ind2 < 0)    return 1;
+    //     if(ind2 < 0 && ind1 >= 0)   return 0;
+    //     if(ind1 < 0){
+    //         for(int i = 0; i <= ind2; i++){
+    //             if(p.charAt(i) != '*'){
+    //                 return 0;
+    //             }
+    //         }
+    //         return 1;
+    //     }
+
+    //     if(dp[ind1][ind2] != -1){
+    //         return dp[ind1][ind2];
+    //     }
+
+    //     if(p.charAt(ind2) == '?' || (s.charAt(ind1) == p.charAt(ind2))){
+    //         return dp[ind1][ind2] = f(s, p, ind1 - 1, ind2 - 1, dp);
+    //     }
+
+    //     if(p.charAt(ind2) == '*'){
+    //         int notTake = f(s, p, ind1, ind2 - 1, dp);
+    //         int Take = f(s, p, ind1 - 1, ind2, dp);
+    //         return dp[ind1][ind2] = notTake | Take;
+    //     }
+
+    //     return dp[ind1][ind2] = 0;
+    // }
+
+    // 1 Based Indexing
     private int f(String s, String p, int ind1, int ind2, int[][] dp){
-        if(ind1 < 0 && ind2 < 0)    return 1;
-        if(ind2 < 0 && ind1 >= 0)   return 0;
-        if(ind1 < 0){
-            for(int i = 0; i <= ind2; i++){
-                if(p.charAt(i) != '*'){
+        if(ind1 == 0 && ind2 == 0)    return 1;
+        if(ind2 == 0 && ind1 > 0)   return 0;
+        if(ind1 == 0){
+            for(int i = 1; i <= ind2; i++){
+                if(p.charAt(i - 1) != '*'){
                     return 0;
                 }
             }
@@ -15,11 +46,11 @@ class Solution {
             return dp[ind1][ind2];
         }
 
-        if(p.charAt(ind2) == '?' || (s.charAt(ind1) == p.charAt(ind2))){
+        if(p.charAt(ind2 - 1) == '?' || (s.charAt(ind1 - 1) == p.charAt(ind2 - 1))){
             return dp[ind1][ind2] = f(s, p, ind1 - 1, ind2 - 1, dp);
         }
 
-        if(p.charAt(ind2) == '*'){
+        if(p.charAt(ind2 - 1) == '*'){
             int notTake = f(s, p, ind1, ind2 - 1, dp);
             int Take = f(s, p, ind1 - 1, ind2, dp);
             return dp[ind1][ind2] = notTake | Take;
@@ -29,13 +60,21 @@ class Solution {
     }
 
     public boolean isMatch(String s, String p) {
-        // Memoization Approach
+        // // Memoization Approach 0 Based Indexing
+        // int n = s.length(), m = p.length();
+        // int dp[][] = new int[n][m];
+        // for(int row[] : dp){
+        //     Arrays.fill(row, -1);
+        // }
+        // return f(s, p, n - 1, m - 1, dp) == 1;
+
+        // Memoization Approach 1 Based Indexing
         int n = s.length(), m = p.length();
         int dp[][] = new int[n + 1][m + 1];
         for(int row[] : dp){
             Arrays.fill(row, -1);
         }
-        return f(s, p, n - 1, m - 1, dp) == 1;
+        return f(s, p, n, m, dp) == 1;
 
         // Tabulation Approach
         // int n = s.length(), m = p.length();
