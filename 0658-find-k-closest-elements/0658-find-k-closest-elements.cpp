80f1555d5e1da5e97460b0385636ed1cade2593e
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-31 11:28:59 +0530

    Time: 28 ms (71.54%), Space: 34.4 MB (69.67%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
-        if(k == n){
-            return arr;
-        }
-        
-        vector<int> ans;
         int j = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
         int i = j - 1;
-
-        while(ans.size() < k){
+        int cnt = 0, start_idx = j, end_idx = i;
+        while(cnt < k){
             int a = (i >= 0) ? (a = x - arr[i]) : INT_MAX;
             int b = (j < n) ? (b = arr[j] - x) : INT_MAX;
-            ans.push_back(a <= b ? arr[i--] : arr[j++]);
+            if(a <= b){
+                start_idx = i--;
+            } else{
+                end_idx = j++;
+            }
+            cnt++;
         }
 
-        sort(ans.begin(), ans.end());
+        vector<int> ans;
+        for(int i = start_idx; i <= end_idx; i++){
+            ans.push_back(arr[i]);
+        }
+
         return ans;
     }
 };
