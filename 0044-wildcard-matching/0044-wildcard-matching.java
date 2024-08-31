af3b4cedc3f492acf79cc6d1e10b7161c9c16175
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-08-31 07:09:35 +0530

    Time: 26 ms (17.29%), Space: 44.7 MB (69.19%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -3,7 +3,7 @@ class Solution {
     // private int f(String s, String p, int ind1, int ind2, int[][] dp){
     //     if(ind1 < 0 && ind2 < 0)    return 1;
     //     if(ind2 < 0 && ind1 >= 0)   return 0;
-    //     if(ind1 < 0){
+    //     if(ind1 < 0 && ind2 >= 0){
     //         for(int i = 0; i <= ind2; i++){
     //             if(p.charAt(i) != '*'){
     //                 return 0;
@@ -33,7 +33,7 @@ class Solution {
     private int f(String s, String p, int ind1, int ind2, int[][] dp){
         if(ind1 == 0 && ind2 == 0)    return 1;
         if(ind2 == 0 && ind1 > 0)   return 0;
-        if(ind1 == 0){
+        if(ind1 == 0 && ind2 > 0){
             for(int i = 1; i <= ind2; i++){
                 if(p.charAt(i - 1) != '*'){
                     return 0;
@@ -69,41 +69,48 @@ class Solution {
         // return f(s, p, n - 1, m - 1, dp) == 1;
 
         // Memoization Approach 1 Based Indexing
-        int n = s.length(), m = p.length();
-        int dp[][] = new int[n + 1][m + 1];
-        for(int row[] : dp){
-            Arrays.fill(row, -1);
-        }
-        return f(s, p, n, m, dp) == 1;
-
-        // Tabulation Approach
         // int n = s.length(), m = p.length();
         // int dp[][] = new int[n + 1][m + 1];
-        // dp[0][0] = 1;
-        // for(int ind2 = 1; ind2 <= m; ind2++){
-        //     int i = ind2;
-        //     while(i > 0 && p.charAt(i - 1) == '*'){
-        //         i--;
-        //     }
-        //     dp[0][ind2] = i == 0 ? 1 : 0;
+        // for(int row[] : dp){
+        //     Arrays.fill(row, -1);
         // }
+        // return f(s, p, n, m, dp) == 1;
 
-        // for(int ind1 = 1; ind1 <= n; ind1++){
-        //     for(int ind2 = 1; ind2 <= m; ind2++){
-        //         if(p.charAt(ind2 - 1) == '?'){
-        //             dp[ind1][ind2] = dp[ind1 - 1][ind2 - 1];
-        //         } else if(p.charAt(ind2 - 1) == '*'){
-        //             int flag = 0;
-        //             for(int i = ind1; i >= 0; i--){
-        //                 flag |= dp[i][ind2 - 1];
-        //             }
-        //             dp[ind1][ind2] = flag;
-        //         } else{
-        //             dp[ind1][ind2] = (s.charAt(ind1 - 1) == p.charAt(ind2 - 1) ? 1 : 0) & dp[ind1 - 1][ind2 - 1];
-        //         }
-        //     }
-        // }
+        // Tabulation Approach
+        int n = s.length(), m = p.length();
+        boolean dp[][] = new boolean[n + 1][m + 1];
 
-        // return dp[n][m] == 1;
+        dp[0][0] = true;
+
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            dp[ind1][0] = false;
+        }
+
+        for(int ind2 = 1; ind2 <= m; ind2++){
+            boolean flag = true;
+            for(int i = 1; i <= ind2; i++){
+                if(p.charAt(i - 1) != '*'){
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][ind2] = flag;
+        }
+
+        for(int ind1 = 1; ind1 <= n; ind1++){
+            for(int ind2 = 1; ind2 <= m; ind2++){
+                if(p.charAt(ind2 - 1) == '?' || (s.charAt(ind1 - 1) == p.charAt(ind2 - 1))){
+                    dp[ind1][ind2] = dp[ind1 - 1][ind2 - 1];
+                } else if(p.charAt(ind2 - 1) == '*'){
+                    boolean notTake = dp[ind1][ind2 - 1];
+                    boolean Take = dp[ind1 - 1][ind2];
+                    dp[ind1][ind2] = notTake || Take;
+                } else{
+                    dp[ind1][ind2] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
