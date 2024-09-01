e14e96e1b490e93a8f957b62c7fa9dfd3d1f7c93
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-01 07:58:43 +0530

    Time: 385 ms (11.64%), Space: 79.8 MB (6.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,31 @@
 class Solution {
     private int f(int[] nums, int ind, int prev_ind, int dp[][]){
-        if(ind - 1 == nums.length){
+        if(ind == nums.length){
             return 0;
         }
 
-        if(dp[ind][prev_ind] != -1){
-            return dp[ind][prev_ind];
+        if(dp[ind][prev_ind + 1] != -1){
+            return dp[ind][prev_ind + 1];
         }
 
         int notPick = f(nums, ind + 1, prev_ind, dp), pick = 0;
 
-        if(prev_ind == 0 || nums[ind - 1] > nums[prev_ind - 1]){
+        if(prev_ind == -1 || nums[ind] > nums[prev_ind]){
             pick = 1 + f(nums, ind + 1, ind, dp);
         }
 
-        return dp[ind][prev_ind] = Math.max(pick, notPick);
+        return dp[ind][prev_ind + 1] = Math.max(pick, notPick);
     }
 
     public int lengthOfLIS(int[] nums) {
         // Memoization Approach
         int n = nums.length;
-        int dp[][] = new int[n + 1][n + 1];
+        int dp[][] = new int[n][n + 1];
 
         for(int row[] : dp){
             Arrays.fill(row, -1);
         }
 
-        return f(nums, 1, 0, dp);
+        return f(nums, 0, -1, dp);
     }
 }
