d19cb1fe47a2b3e3ee49567a5f87ca88c62aedbf
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-04 09:30:43 +0530

    Time: 94 ms (43.09%), Space: 57.2 MB (7.18%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -1,37 +1,69 @@
+// // Mine Code
+// class Solution {
+// public:
+//     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+//         unordered_map<int, unordered_set<int>> st;
+//         for(auto it : obstacles){
+//             st[it[0]].insert(it[1]);
+//         }
+
+//         int x = 0, y = 0;
+//         int val = 1, ans = 0;
+//         bool yAxis = true;
+//         for(int i : commands){
+//             if(i == -1){
+//                 yAxis = !yAxis;
+//                 val *= (yAxis ? -1 : 1);
+//             } else if(i == -2){
+//                 val *= (yAxis ? -1 : 1);
+//                 yAxis = !yAxis;
+//             } else{
+//                 if(yAxis){
+//                     for(int j = 1; j <= i; j++){
+//                         if(st[x].count(y + val)){
+//                             break;
+//                         }
+//                         y += val;                      
+//                     }
+//                 } else{
+//                     for(int j = 1; j <= i; j++){
+//                         if(st[x + val].count(y)){
+//                             break;
+//                         }
+//                         x += val;
+//                     }
+//                 }
+//                 ans = max(ans, (int)(pow(x, 2) + pow(y, 2)));
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// In Well Structured Manner
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        unordered_map<int, unordered_set<int>> ax, ay;
+        unordered_map<int, unordered_set<int>> st;
         for(auto it : obstacles){
-            ax[it[0]].insert(it[1]);
-            ay[it[1]].insert(it[0]);
+            st[it[0]].insert(it[1]);
         }
 
-        int x = 0, y = 0;
-        int val = 1, ans = 0;
-        bool yAxis = true;
+        vector<vector<int>> dir({{0, 1}, {1, 0}, {0, -1}, {-1, 0}});
+        int x = 0, y = 0, d = 0, ans = 0;
+
         for(int i : commands){
             if(i == -1){
-                yAxis = !yAxis;
-                val *= (yAxis ? -1 : 1);
+                d = (d + 1) % 4;
             } else if(i == -2){
-                val *= (yAxis ? -1 : 1);
-                yAxis = !yAxis;
+                d = (d + 4 - 1) % 4;
             } else{
-                if(yAxis){
-                    for(int j = 1; j <= i; j++){
-                        if(ax[x].count(y + val)){
-                            break;
-                        }
-                        y += val;                      
+                int dx = dir[d][0], dy = dir[d][1];
+                for(int k = 1; k <= i; k++){
+                    if(st[x + dx].count(y + dy)){
+                        break;
                     }
-                } else{
-                    for(int j = 1; j <= i; j++){
-                        if(ay[y].count(x + val)){
-                            break;
-                        }
-                        x += val;
-                    }
+                    x += dx, y += dy;
                 }
                 ans = max(ans, (int)(pow(x, 2) + pow(y, 2)));
             }
