b758ffcf24ec0c87a80617229b61d4740bb8c4ab
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-09 17:52:39 +0530

    Time: 46 ms (87.48%), Space: 34.1 MB (87.1%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,36 +2,37 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        
-        vector<vector<int>> ans(n, vector<int>(m, 0));
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        vector<vector<int>> ans(n, vector<int>(m, -1));
         queue<pair<int,int>> q;
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 if(mat[i][j] == 0){
                     q.push({i, j});
-                    vis[i][j] = true;
-                }
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
-        int dis = 0;
-        while(!q.empty()){
-            dis++;
-            int size = q.size();
-            while(size--){
-                int row = q.front().first, col = q.front().second;
-                q.pop();
-                for(int i = 0; i < 4; i++){
-                    int nrow = row + delrow[i];
-                    int ncol = col + delcol[i];
-                    if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol]){
-                        vis[nrow][ncol] = true;
-                        ans[nrow][ncol] = dis;
-                        q.push({nrow, ncol});
-                    }
+                    ans[i][j] = 0;
                 }
             }
         }
+
+        int delRow[] = {0, 1, 0, -1};
+        int delCol[] = {1, 0, -1, 0};
+        int cnt = 1;
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                auto [x, y] = q.front();
+                q.pop();
+                for(int i = 0; i < 4; i++){
+                    int dx = x + delRow[i];
+                    int dy = y + delCol[i];
+                    if(dx >= 0 && dy >= 0 && dx < n && dy < m && ans[dx][dy] == -1){
+                        ans[dx][dy] = cnt;
+                        q.push({dx, dy});
+                    }
+                }
+            }
+            cnt++;
+        }
+        
         return ans;
     }
 };
