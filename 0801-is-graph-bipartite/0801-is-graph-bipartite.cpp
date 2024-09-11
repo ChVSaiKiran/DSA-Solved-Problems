c1f0815cd108f28d2ff7f94085dca9e0f675464a
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-11 14:13:15 +0530

    Time: 26 ms (8.8%), Space: 18.8 MB (12.36%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,29 +1,50 @@
 class Solution {
+private:
+    bool bfs(vector<vector<int>> graph, int u, vector<int> &color){
+        queue<int> q({u});
+        color[u] = 0;
+        while(!q.empty()){
+            int u = q.front(); q.pop();
+            for(int v : graph[u]){
+                if(color[v] == color[u]){
+                    return false;
+                } else if(color[v] == -1){
+                    color[v] = !color[u];
+                    q.push(v);
+                }
+            }
+        }
+        return true;
+    }
+
+    bool dfs(vector<vector<int>>& graph, int u, vector<int> &arr){
+        bool flag = true;
+        for(int v : graph[u]){
+            if(arr[v] == -1){
+                arr[v] = !arr[u];
+                flag = dfs(graph, v, arr) && flag;
+            } else if(arr[v] == arr[u]){
+                return false;
+            }
+        }
+        return flag;
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> color(n, -1);
-
         for(int i = 0; i < n; i++){
             if(color[i] == -1){
-                queue<int> q;
-                q.push(i);
-                color[i] = 0;
-                while(!q.empty()){
-                    int u = q.front(), c = !color[u];
-                    q.pop();
-                    for(int v : graph[u]){
-                        if(color[v] == -1){
-                            color[v] = c;
-                            q.push(v);
-                        } else if(color[v] != c){
-                            return false;
-                        }
-                    }
+                if(bfs(graph, i, color) == false){
+                    return false;
                 }
+
+                // if(dfs(graph, i, color) == false){
+                //     return false;
+                // }
             }
         }
-
         return true;
     }
 };
