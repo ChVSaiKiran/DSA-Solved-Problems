cff518a2daf91b7582ef81a21d09b08986fe3368
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-14 14:58:31 +0530

    Time: 96 ms (82.68%), Space: 84.8 MB (90.79%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int val = nums[0], max_val = nums[0];
+        int val = nums[0], max_val = nums[0], max_val_len = 1;
         int n = nums.size(), j = 0;
-        unordered_map<int, int> mp{{val, 1}};
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n; i++){
             int temp = val & nums[i];
             if(nums[i] > temp){
-                val = nums[i];
-                j = i;
+                val = nums[i], j = i;
             } else{
                 val = temp;
             }
-            max_val = max(max_val, val);
-            mp[val]  = max(mp[val], i - j + 1);
+
+            int len = i - j + 1;
+
+            if(val > max_val){
+                max_val = val;
+                max_val_len = 1;
+            } else if(val == max_val){
+                max_val_len = max(max_val_len, len);
+            }
         }
 
-        return mp[max_val];
+        return max_val_len;
     }
 };
