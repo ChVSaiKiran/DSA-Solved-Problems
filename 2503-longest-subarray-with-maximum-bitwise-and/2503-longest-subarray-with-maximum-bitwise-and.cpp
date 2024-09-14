a5d1ce4f8e1f284bf6d7183d94288da417692919
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-14 14:36:04 +0530

    Time: 145 ms (5.26%), Space: 93.6 MB (5.04%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -6,15 +6,17 @@ public:
         unordered_map<int, int> mp{{val, 1}};
 
         for(int i = 0; i < n; i++){
-            if((val & nums[i]) < nums[i]){
+            int temp = val & nums[i];
+            if(nums[i] > temp){
                 val = nums[i];
                 j = i;
+            } else{
+                val = temp;
             }
-            val = val & nums[i];
-            mp[val]  = max(mp[val], i - j + 1);
             max_val = max(max_val, val);
+            mp[val]  = max(mp[val], i - j + 1);
         }
-        
+
         return mp[max_val];
     }
 };
