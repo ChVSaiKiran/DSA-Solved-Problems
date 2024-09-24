fcccccfe03968f72485fc39081a1c60c58ebb881
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-09-24 12:09:45 +0530

    Time: 54 ms (16.92%), Space: 21.6 MB (86.68%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        long val = 1;
-        vector<int> ones(32, 0), zeros(32, 0);
+        vector<int> zeros(32, 0);
         for(int i = 0; i < 32; i++){
+            long val = pow(2, i);
             for(int x : nums){
-                if(x & val){
-                    ones[i]++;
-                } else{
+                if((x & val) == 0){
                     zeros[i]++;
                 }
             }
-            val *= 2;
         }
 
-        val = 1;
         int ans = 0;
         for(int i = 0; i < 32; i++){
+            long val = pow(2, i);
             for(int x : nums){
                 if(x & val){
                     ans += zeros[i];
                 }
             }
-            val *= 2;
         }
 
         return ans;
