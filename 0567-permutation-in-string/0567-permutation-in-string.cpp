644d4dff3c0ab375dab0afd15c1e42871809fd34
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-10-05 07:37:39 +0530

    Time: 5 ms (70.09%), Space: 8.6 MB (92.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,22 +1,29 @@
 class Solution {
+    bool check(vector<int> &a, vector<int> &b){
+        for(int i = 0; i < 26; i++){
+            if(a[i] != b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.length() > s2.length())
-            return false;
-        unordered_map<char,int> mp1, mp2;
-        for(char i:s1)
-            mp1[i]++;
-        for(int i = 0; i < s1.length(); i++)
-            mp2[s2[i]]++;
-        int n = s1.length(), i = n;
-        for(int i = n; i < s2.length(); i++){
-            if(mp1 == mp2)
-                return true;
-            mp2[s2[i - n]]--;
-            if(mp2[s2[i - n]] == 0)
-                mp2.erase(s2[i - n]);
-            mp2[s2[i]]++;
+        vector<int> a(26, 0), b(26, 0);
+        for(char i : s1){
+            a[i - 'a']++;
         }
-        return mp1 == mp2;
+
+        for(int i = 0; i < s2.size(); i++){
+            b[s2[i] - 'a']++;
+            if(i >= s1.size() - 1){
+                if(check(a, b)){
+                    return true;
+                }
+                b[s2[i - s1.size() + 1] - 'a']--;
+            }
+        }
+
+        return false;
     }
 };
