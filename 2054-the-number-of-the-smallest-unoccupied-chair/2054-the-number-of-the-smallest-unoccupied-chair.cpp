08402555ca99686ca2573fd9454eb15f0b58b413
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2024-10-11 21:11:44 +0530

    Time: 955 ms (5.16%), Space: 61.8 MB (67.01%) - LeetHub

---
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int tar = times[targetFriend][0];
+        sort(times.begin(), times.end());
+
+        map<int,int> mp;
+        for(auto it : times){
+            int sTime = it[0], eTime = it[1];
+            bool flag = true;
+
+            for(auto &[chair_num, chair_eTime] : mp){
+                if(chair_eTime <= sTime){
+                    if(sTime == tar){
+                        return chair_num;
+                    }
+                    chair_eTime = it[1];
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag){
+                if(sTime == tar){
+                    return mp.size();
+                }
+                mp[mp.size()] = eTime;
+            }
+        }
+
+        return -1;
+    }
+};
