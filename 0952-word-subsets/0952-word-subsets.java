a9aea0336a6989b56dd59456958ddf2ee9fa34f5
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-01-11 05:29:44 +0530

    Time: 13 ms (81.07%), Space: 55 MB (24.26%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.java
+++ 0952-word-subsets/0952-word-subsets.java
@@ -9,29 +9,27 @@ class Solution {
     }
 
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        int n = words1.length, freq1[][] = new int[n][26];
-        for(int i = 0; i < n; i++){
-            for(char ch : words1[i].toCharArray()){
-                freq1[i][ch - 'a']++;
-            }
-        }
-
-        int m = words2.length, freq2[] = new int[26];
+        int n = words1.length, m = words2.length, req[] = new int[26];
         for(int i = 0; i < m; i++){
             int temp[] = new int[26];
             for(char ch : words2[i].toCharArray()){
                 temp[ch - 'a']++;
-                freq2[ch - 'a'] = Math.max(freq2[ch - 'a'], temp[ch - 'a']);
+                req[ch - 'a'] = Math.max(req[ch - 'a'], temp[ch - 'a']);
             }
         }
         
         List<String> ans = new ArrayList<>();
         for(int i = 0; i < n; i++){
-            if(fun(freq1[i], freq2)){
+            int curr[] = new int[26];
+            for(char ch : words1[i].toCharArray()){
+                curr[ch - 'a']++;
+            }
+            if(fun(curr, req)){
                 ans.add(words1[i]);
             }
-        }
 
+        }
+        
         return ans;
     }
 }
