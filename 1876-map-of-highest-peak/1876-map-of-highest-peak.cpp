102ce72822a084ec88c13cc454e8f2f75492e523
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-01-22 09:06:48 +0530

    Time: 64 ms (90.29%), Space: 112.6 MB (91.38%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        queue<pair<int, int>> q;
+        int n = isWater.size(), m = isWater[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(isWater[i][j] == 1){
+                    int idx = (i * m) + j;
+                    q.push({idx, 0});
+                    ans[i][j] = 0;
+                }
+            }
+        }
+
+        int delRow[] = {0, 1, 0, -1};
+        int delCol[] = {1, 0, -1, 0};
+
+        while(!q.empty()){
+            int size = q.size();
+            while(size-- > 0){
+                int idx = q.front().first;
+                int lvl = q.front().second;
+                int r = idx / m, c = idx % m;
+                q.pop();
+
+                for(int i = 0; i < 4; i++){
+                    int nx = r + delRow[i];
+                    int ny = c + delCol[i];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] > lvl + 1){
+                        ans[nx][ny] = lvl + 1;
+                        q.push({(nx * m) + ny, lvl + 1});
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
