fc5e9f155beeb2556092742569c5c8d29a3f4673
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-02-23 00:02:45 +0530

    Time: 0 ms (100%), Space: 21 MB (71.12%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void fun(TreeNode *root, vector<pair<int,int>> &temp, int d, int &idx){
+        if(idx < temp.size() && temp[idx].second == d + 1){
+            root -> left = new TreeNode(temp[idx].first);
+            idx++;
+            fun(root -> left, temp, d + 1, idx);
+        }
+        
+        if(idx < temp.size() && temp[idx].second == d + 1){
+            root -> right = new TreeNode(temp[idx].first);
+            idx++;
+            fun(root -> right, temp, d + 1, idx);
+        }
+    }
+
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        vector<pair<int,int>> temp;
+        int cnt = 0, i = 0, n = traversal.size();
+        char prev = '-';
+        while(i < n){
+            if(traversal[i] == '-'){
+                cnt++; i++; continue;
+            }
+            
+            int val = 0;
+            while(i < n && isdigit(traversal[i])){
+                val = (val * 10) + (traversal[i++] - '0');
+            }
+
+            temp.push_back({val, cnt});
+            cnt = 0;
+        }
+
+        TreeNode *root = new TreeNode(temp[0].first);
+        int idx = 1;
+        fun(root, temp, 0, idx);
+        return root;
+    }
+};
