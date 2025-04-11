14b10d475abd4316eb810dfae665e4ad8d6425fd
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-04-11 09:11:55 +0530

    Time: 6 ms (94.91%), Space: 34.7 MB (77.04%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, -1));
-        queue<pair<int,int>> q;
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {        
+        int n = mat.size();
+        int m = mat[0].size();
+
+        queue<pair<int, int>> q;
+        vector<vector<int>> ans(n, vector<int> (m));
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(mat[i][j] == 0){
-                    q.push({i, j});
                     ans[i][j] = 0;
+                    q.push({i, j});
+                } else{
+                    ans[i][j] = INT_MAX;
                 }
             }
         }
 
         int delRow[] = {0, 1, 0, -1};
         int delCol[] = {1, 0, -1, 0};
-        int cnt = 1;
         while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto [x, y] = q.front();
-                q.pop();
-                for(int i = 0; i < 4; i++){
-                    int dx = x + delRow[i];
-                    int dy = y + delCol[i];
-                    if(dx >= 0 && dy >= 0 && dx < n && dy < m && ans[dx][dy] == -1){
-                        ans[dx][dy] = cnt;
-                        q.push({dx, dy});
+            int x = q.front().first;
+            int y = q.front().second;
+            for(int i = 0; i < 4; i++){
+                int nx = x + delRow[i];
+                int ny = y + delCol[i];
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
+                    if(ans[nx][ny] == INT_MAX){
+                        q.push({nx, ny});
                     }
+                    ans[nx][ny] = min(ans[nx][ny], ans[x][y] + 1);
                 }
             }
-            cnt++;
+            q.pop();
         }
-        
         return ans;
     }
 };
