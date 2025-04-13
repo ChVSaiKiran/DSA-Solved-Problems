e9cc2bbb6a19742c78de1abec642dccd08f8d29f
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-04-13 18:53:23 +0530

    Time: 0 ms (100%), Space: 17.2 MB (78.82%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,48 +1,28 @@
 class Solution {
 private:
-    bool bfs(vector<vector<int>> graph, int u, vector<int> &color){
-        queue<int> q({u});
-        color[u] = 0;
-        while(!q.empty()){
-            int u = q.front(); q.pop();
-            for(int v : graph[u]){
-                if(color[v] == color[u]){
+    bool cancolour(vector<vector<int>>& graph, int u, vector<int> &colour){
+        for(int v : graph[u]){
+            if(colour[v] == -1){
+                colour[v] = !colour[u];
+                if(cancolour(graph, v, colour) == false){
                     return false;
-                } else if(color[v] == -1){
-                    color[v] = !color[u];
-                    q.push(v);
                 }
+            } else if(colour[v] == colour[u]){
+                return false;
             }
         }
         return true;
     }
-
-    bool dfs(vector<vector<int>>& graph, int u, vector<int> &arr){
-        bool flag = true;
-        for(int v : graph[u]){
-            if(arr[v] == -1){
-                arr[v] = !arr[u];
-                flag = dfs(graph, v, arr) && flag;
-            } else if(arr[v] == arr[u]){
-                return false;
-            }
-        }
-        return flag;
-    }
-
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n, -1);
+        vector<int> colour(n, -1);
         for(int i = 0; i < n; i++){
-            if(color[i] == -1){
-                if(bfs(graph, i, color) == false){
+            if(colour[i] == -1){
+                colour[i] = 0;
+                if(cancolour(graph, i, colour) == false){
                     return false;
                 }
-
-                // if(dfs(graph, i, color) == false){
-                //     return false;
-                // }
             }
         }
         return true;
