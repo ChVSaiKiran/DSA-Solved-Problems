789fb9593d1c1c388ee11c9decda3ace1cf0b693
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-04-14 09:02:14 +0530

    Time: 7 ms (36.59%), Space: 18.2 MB (65.12%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,29 +1,35 @@
 class Solution {
-    vector<int> ans;
-    bool dfs(int V, vector<int> adj[], vector<int> &vis, int u){
-        vis[u] = 2;
-        for(int v: adj[u]){
-            if(vis[v] == 0){
-                if(dfs(V, adj, vis, v))
-                    return true;
-            }
-            else if(vis[v] == 2)
-                return true;
-        }
-        vis[u] = 1;
-        ans.push_back(u);
-        return false;
-    }
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses, 0);
-        vector<int> adj[numCourses];
-        for(auto it:prerequisites)
-            adj[it[0]].push_back(it[1]);
-        for(int i = 0; i < numCourses; i++)
-            if(vis[i] == 0)
-                if(dfs(numCourses, adj, vis, i))
-                    return {};
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        vector<int> adj[n];
+        vector<int> ind(n, 0);
+        for(auto it : prerequisites){
+            ind[it[0]]++;
+            adj[it[1]].push_back(it[0]);
+        }
+
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(ind[i] == 0){
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+        while(!q.empty()){
+            int u = q.front();  q.pop();
+            ans.push_back(u);
+
+            for(int v : adj[u]){
+                ind[v]--;
+                if(ind[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+
+        if(ans.size() != n) ans.clear();
+
         return ans;
     }
 };
