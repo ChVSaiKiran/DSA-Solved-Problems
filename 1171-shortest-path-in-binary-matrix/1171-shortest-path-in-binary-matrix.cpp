3e87570ab789e74df8cf3cc28284bfc2022d54a7
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-04-17 07:03:52 +0530

    Time: 15 ms (76.81%), Space: 24.4 MB (70.43%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(grid[0][0] == 1 || grid[n - 1][n - 1] == 1){
+        int n = grid.size(), m = grid[0].size();
+        if(grid[0][0] == 1){
             return -1;
         }
 
-        vector<vector<int>> dis(n, vector<int>(n, 1e9));
+        vector<vector<int>> dis(n, vector<int>(m, 1e9));
+        int nx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+        int ny[] = {1, 1, 0, -1, -1, -1, 0, 1};
+        queue<int> q;
+        q.push(0);
         dis[0][0] = 1;
 
-        int delRow[] = {0, 1, 1, 1, 0, -1, -1, -1};
-        int delCol[] = {1, 1, 0, -1, -1, -1, 0, 1};
-        queue<pair<int, int>> q;
-        q.push({0, 0});
-
         while(!q.empty()){
-            auto [x, y] = q.front();
-            q.pop();
-
+            int u = q.front(); q.pop();
+            int x = u / m, y = u % m;
             for(int i = 0; i < 8; i++){
-                int dx = x + delRow[i], dy = y + delCol[i];
-                if(dx >= 0 && dy >= 0 && dx < n && dy < n){
-                    if(grid[dx][dy] == 0 && dis[dx][dy] > dis[x][y] + 1){
-                        dis[dx][dy] = dis[x][y] + 1;
-                        q.push({dx, dy});
-                    }
+                int dx = x + nx[i];
+                int dy = y + ny[i];
+                if(dx >= 0 && dy >= 0 && dx < n && dy < m && grid[dx][dy] == 0 && dis[x][y] + 1 < dis[dx][dy]){
+                    dis[dx][dy] = dis[x][y] + 1;
+                    q.push(dx * m + dy);
                 }
             }
         }
 
-        return dis[n - 1][n - 1] == 1e9 ? -1 : dis[n - 1][n - 1];
+        return dis[n - 1][m - 1] == 1e9 ? -1 : dis[n - 1][m - 1];
     }
 };
