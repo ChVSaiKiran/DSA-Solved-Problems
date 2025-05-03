31f1e6d532b6be5061c184bd1578423f33da8a23
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-05-03 09:25:38 +0530

    Time: 7 ms (34.91%), Space: 115.2 MB (99.57%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        vector<int> a(6, 0), b(6, 0);
         int n = tops.size(), ans = INT_MAX;
-        for(int val = 1; val <= 6; val++){
-            int cnt1 = 0, cnt2 = 0, flag = 1;
-            for(int i = 0; i < n; i++){
-                if(tops[i] != val && bottoms[i] != val){
-                    flag = 0;    break;
-                }
-                cnt1 += (tops[i] != val);
-                cnt2 += (bottoms[i] != val);
-            }
-            if(flag){
-                ans = min(ans, min(cnt1, cnt2));
-            }
+        for(int i = 0; i < n; i++){
+            a[tops[i] - 1]++;   b[bottoms[i] - 1]++;
         }
+
+        int val1 = 1, val2 = 1;
+        for(int i = 1; i < 6; i++){
+            if(a[i] > a[val1 - 1])  val1 = i + 1;
+
+            if(b[i] > b[val2 - 1])  val2 = i + 1;
+        }
+
+        int val = a[val1 - 1] > b[val2 - 1] ? val1 : val2;
+
+        int cnt1 = 0, cnt2 = 0, flag = 1;
+        for(int i = 0; i < n; i++){
+            if(tops[i] != val && bottoms[i] != val){
+                flag = 0;    break;
+            }
+            cnt1 += (tops[i] != val);
+            cnt2 += (bottoms[i] != val);
+        }
+
+        if(flag){
+            ans = min(ans, min(cnt1, cnt2));
+        }
+
         if(ans == INT_MAX)  ans = -1;
         return ans;
     }
