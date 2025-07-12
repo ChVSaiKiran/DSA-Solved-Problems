cc11bebce2c7bbdc37766daf2527c2497d65d6d4
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-07-12 11:57:47 +0530

    Time: 14 ms (78.36%), Space: 34.8 MB (70.66%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {        
-        int n = mat.size();
-        int m = mat[0].size();
-
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         queue<pair<int, int>> q;
-        vector<vector<int>> ans(n, vector<int> (m));
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, 1e9));
+
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(mat[i][j] == 0){
                     ans[i][j] = 0;
                     q.push({i, j});
-                } else{
-                    ans[i][j] = INT_MAX;
                 }
             }
         }
 
         int delRow[] = {0, 1, 0, -1};
         int delCol[] = {1, 0, -1, 0};
+
         while(!q.empty()){
             int x = q.front().first;
-            int y = q.front().second;
+            int y = q.front().second;   q.pop();
             for(int i = 0; i < 4; i++){
                 int nx = x + delRow[i];
                 int ny = y + delCol[i];
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m){
-                    if(ans[nx][ny] == INT_MAX){
-                        q.push({nx, ny});
-                    }
-                    ans[nx][ny] = min(ans[nx][ny], ans[x][y] + 1);
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] > ans[x][y] + 1){
+                    ans[nx][ny] = ans[x][y] + 1;
+                    q.push({nx, ny});
                 }
             }
-            q.pop();
         }
+
         return ans;
     }
 };
