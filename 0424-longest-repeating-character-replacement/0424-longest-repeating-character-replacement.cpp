e40e384f3d8eebb9e61d3009ee0920ac674459e1
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-08-02 07:54:09 +0530

    Time: 31 ms (6.93%), Space: 10.8 MB (92.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,24 +1,17 @@
 class Solution {
-private:
-    int fun(string &s, int k, int ch){
-        int j = 0, cnt = 0;
-        for(int i = 0; i < s.length(); i++){
-            while(s[i] != ch && k == 0){
-                k += (s[j++] != ch);
-            }
-            k -= (s[i] != ch);
-            cnt = max(cnt, i - j + 1);
-        }
-        return cnt;
-    }
 public:
     int characterReplacement(string s, int k) {
-        int ans = 0;
-        vector<bool> vis(26, false);
-        for(char ch : s){
-            if(vis[ch - 'A'])  continue;
-            vis[ch - 'A'] = true;
-            ans = max(ans, fun(s, k, ch));
+        int ans = 0, n = s.length();
+        for(char ch = 'A'; ch <= 'Z'; ch++){
+            int j = 0, cnt = k;
+            for(int i = 0; i < n; i++){
+                cnt -= (s[i] != ch);
+                while(cnt < 0){
+                    cnt += (s[j] != ch); 
+                    j++;
+                }
+                ans = max(ans, i - j + 1);
+            }
         }
         return ans;
     }
