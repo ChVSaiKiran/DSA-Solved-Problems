f531645ccefb80eaa72412013e10185446697613
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-08-02 07:18:03 +0530

    Time: 24 ms (28.5%), Space: 27.7 MB (22.42%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,30 @@
-class Solution
-{
-    public:
-        vector<vector < string>> groupAnagrams(vector<string> &strs)
-        {
-            unordered_map<string, vector < string>> mp;
-            for (string s: strs)
-            {
-                string t = s;
-                sort(t.begin(), t.end());
-                mp[t].push_back(s);
-            }
-            vector<vector < string>> res;
-            res.reserve(mp.size());
-            for (auto it: mp)
-                res.push_back(it.second);
-            return res;
+class Solution {
+private:
+    size_t getHash(string &str){
+        vector<int> freq(26, 0);
+        for(char c : str)  freq[c - 'a']++;
+
+        int multiplier = 31;
+        size_t hashValue = 0;
+        for(int i : freq){
+            hashValue = hashValue * multiplier + hash<int>{}(i);
         }
+        return hashValue;
+    }
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<size_t, vector<string>> mp;
+        for(string str : strs){
+            mp[getHash(str)].push_back(str);
+        }
+
+        vector<vector<string>> ans;
+        ans.reserve(mp.size());
+
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
 };
