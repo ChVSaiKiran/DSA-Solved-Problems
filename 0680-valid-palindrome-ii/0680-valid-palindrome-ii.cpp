84f39204342d450237b2f013234990959d0889cc
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2025-08-02 07:36:51 +0530

    Time: 8 ms (26.51%), Space: 26.8 MB (24.36%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,15 +1,21 @@
 class Solution {
-    bool valid(string s, int lo, int hi){
-        for(int i = lo, j = hi; i < j; i++, j--)
-            if(s[i] != s[j])
+    bool isValid(string s, int i, int j){
+        while(i <= j){
+            if(s[i] != s[j]){
                 return false;
+            }
+            i++, j--;
+        }
         return true;
     }
+
 public:
     bool validPalindrome(string s) {
-        for(int i = 0, j = s.length() - 1; i < j; i++, j--)
-            if(s[i] != s[j])
-                return valid(s, i + 1, j) || valid(s, i, j - 1);
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--){
+            if(s[i] != s[j]){
+                return isValid(s, i + 1, j) || isValid(s, i, j - 1);
+            }
+        }
         return true;
     }
 };
