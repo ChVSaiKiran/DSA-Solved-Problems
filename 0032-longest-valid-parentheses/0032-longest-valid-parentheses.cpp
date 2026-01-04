a5a1a6966cab2362a455fa0945d0fabc9bc3bd63
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-01-04 23:20:42 +0530

    Time: 11 ms (5.53%), Space: 15.2 MB (5.22%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -10,11 +10,18 @@ public:
                 stk.push({ch, i});
             } else if(stk.size() != 0 && stk.top().first == '('){
                 pair<char, int> top = stk.top(); stk.pop();
-                int l = top.second, r = i;
+                int l = top.second, r = i, len = 2;
 
-                int l1 = mp[l - 1], l2 = mp[r - 1];
-                int len = l1 + l2 + 2;  ans = max(ans, len);
+                if(mp.count(l - 1)){
+                    len += mp[l - 1];   mp.erase(l - 1);
+                }
+
+                if(mp.count(r - 1)){
+                    len += mp[r - 1];   mp.erase(r - 1);
+                }
+
                 mp[r] = len;
+                ans = max(ans, len);
             } else{
                 stk.push({ch, i});
             }
