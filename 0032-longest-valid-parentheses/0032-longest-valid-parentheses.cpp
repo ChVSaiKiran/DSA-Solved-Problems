bbca53d2ec73582a3184f0a88b851f68184a5051
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-01-04 23:17:35 +0530

    Time: 15 ms (5.53%), Space: 17.5 MB (5.22%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int ans = 0;
-        stack<int> stk;
-        vector<int> mp(s.length() + 1, 0);
+        unordered_map<int, int> mp;
+        stack<pair<char, int>> stk;
         for(int i = 0; i < s.length(); i++){
-            if(stk.empty() || s[i] == '('){
-                stk.push(i);
-            } else if(s[stk.top()] == '('){
-                int sIdx = stk.top();   stk.pop();
-                int len = i - sIdx + 1;
-                ans = max(ans, len + mp[sIdx]);
-                mp[i + 1] = len + mp[sIdx];
+            char ch = s[i];
+            if(ch == '('){
+                stk.push({ch, i});
+            } else if(stk.size() != 0 && stk.top().first == '('){
+                pair<char, int> top = stk.top(); stk.pop();
+                int l = top.second, r = i;
+
+                int l1 = mp[l - 1], l2 = mp[r - 1];
+                int len = l1 + l2 + 2;  ans = max(ans, len);
+                mp[r] = len;
             } else{
-                stk.push(i);
+                stk.push({ch, i});
             }
         }
+
         return ans;
     }
 };
