d453b7f729b6a980b25e72161a3595c56d6e25b7
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-02-05 07:44:39 +0530

    Time: 19 ms (56.07%), Space: 34.8 MB (64.54%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<int, int>> q;
         int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, 1e9));
+        vector<vector<int>> ans(n, vector<int>(m, INT_MAX));
 
+        queue<pair<int, int>> q;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(mat[i][j] == 0){
-                    ans[i][j] = 0;
-                    q.push({i, j});
+                    ans[i][j] = 0;  q.push({i, j});
                 }
             }
         }
 
-        int delRow[] = {0, 1, 0, -1};
-        int delCol[] = {1, 0, -1, 0};
-
+        int row[4] = {0, 1, 0, -1};
+        int col[4] = {1, 0, -1, 0};
         while(!q.empty()){
-            int x = q.front().first;
-            int y = q.front().second;   q.pop();
-            for(int i = 0; i < 4; i++){
-                int nx = x + delRow[i];
-                int ny = y + delCol[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && ans[nx][ny] > ans[x][y] + 1){
-                    ans[nx][ny] = ans[x][y] + 1;
-                    q.push({nx, ny});
+            int x = q.front().first, y = q.front().second;  q.pop();
+            for(int i = 0 ; i < 4; i++){
+                int dx = x + row[i];    int dy = y + col[i];
+                if(dx >= 0 && dy >= 0 && dx < n && dy < m && mat[dx][dy] == 1 && ans[dx][dy] > ans[x][y] + 1){
+                    ans[dx][dy] = ans[x][y] + 1;
+                    q.push({dx, dy});
                 }
             }
         }
