d65de4b4d77ccaf27c6c9cee8622fd4fff17e3d2
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-02-08 10:26:55 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.66%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,22 +1,23 @@
 class Solution {
 private:
-    int f(vector<int> &nums, int target, bool flag){
-        int n = nums.size();
-        int l = 0, r = n - 1, idx = - 1;
+    int f(vector<int>& nums, int target, bool flag){
+        int n = nums.size(), l = 0, r = n - 1, ans = -1;
         while(l <= r){
-            int m = l + (r - l) / 2;
-            if(nums[m] == target){
-                idx = m;
-                if(flag)    r = m - 1;
-                else        l = m + 1;
+            int m = (l + r)/2;
+            if(nums[m] > target){
+                r = m - 1;
             } else if(nums[m] < target){
                 l = m + 1;
-            } else{
-                r = m - 1;
+            } else if(flag){
+                r = m - 1;  ans = m;
+            } else{ 
+                l = m + 1;  ans = m;
             }
         }
-        return idx;
+
+        return ans;
     }
+
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         return {f(nums, target, true), f(nums, target, false)};
