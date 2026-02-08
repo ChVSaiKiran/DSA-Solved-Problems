973d3929ca3e22085b916aec8aafb18984f8c995
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-02-08 10:22:53 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.1%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        // Method 1
-        // Smallest number greater than target
-        int low = 0, high = nums.size() - 1, ind = high + 1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            if(nums[mid] == target){
-                return mid;
-            } else if(nums[mid] > target){
-                ind = mid;
-                high = mid - 1;
+        int l = 0, r = nums.size() - 1, ans = -1;
+        while(l <= r){
+            int m = (l + r) / 2;
+            if(nums[m] == target){
+                return m;
+            } 
+            
+            if(nums[m] > target){
+                r = m - 1; ans = m;
             } else{
-                low = mid + 1;
+                l = m + 1; ans = m + 1;
             }
         }
-        return ind;
 
-        // Method 2
-        // largest number less than target and add + 1
-        // int searchInsert(vector<int>& nums, int target) {
-        // int low = 0, high = nums.size() - 1, ind = -1;
-        // while(low <= high){
-        //     int mid = low + (high - low)/2;
-        //     if(nums[mid] == target){
-        //         return mid;
-        //     } else if(nums[mid] > target){
-        //         high = mid - 1;
-        //     } else{
-        //         ind = mid;
-        //         low = mid + 1;
-        //     }
-        // }
-        // return ind + 1;
-        // }
+        return ans;
     }
 };
