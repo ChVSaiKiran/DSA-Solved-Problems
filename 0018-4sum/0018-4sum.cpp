f08907b503006c0b088db8eb5c60571311f57131
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-04-28 07:43:51 +0530

    Time: 16 ms (86.71%), Space: 17.3 MB (82.45%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,46 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
 
-#define ll long long
-class Solution
-{
-    public:
-        vector<vector < int>> fourSum(vector<int> &nums, int target)
-        {
-            int n = nums.size();
-            sort(nums.begin(), nums.end());
-            vector<vector < int>> ans;
-            for (int i = 0; i < n; i++)
-            {
-                if (i > 0 && nums[i] == nums[i - 1])
-                    continue;
-                for (int j = i + 1; j < n; j++)
-                {
-                    if (j > i + 1 && nums[j] == nums[j - 1])
-                        continue;
-                    int lo = j + 1, hi = n - 1;
-                    while (lo < hi)
-                    {
-                        long long sum = nums[i] + nums[j];;
-                        sum += nums[lo];
-                        sum += nums[hi];
-                        if (sum == target)
-                        {
-                            ans.push_back({ nums[i],
-                                nums[j],
-                                nums[lo],
-                                nums[hi] });
-                            lo++; hi--;
-                            while (lo < hi && nums[lo] == nums[lo - 1])
-                                lo++;
-                            while (lo < hi && nums[hi] == nums[hi + 1])
-                                hi--;
+        for(int i = 0; i < n - 3; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            for(int j = i + 1; j < n - 2; j++){
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int k = j + 1, m = n - 1;
+                long val = target;  val -= nums[i]; val -= nums[j];
+                while(k < m){
+                    long sum = nums[k]; sum += nums[m];
+                    if(sum == val){
+                        while(k < m && nums[k] == nums[k + 1]){
+                            k++;
                         }
-                        else if (sum < target)
-                            lo++;
-                        else
-                            hi--;
+
+                        while(k < m && nums[m] == nums[m - 1]){
+                            m--;
+                        }
+
+                        ans.push_back({nums[i], nums[j], nums[k], nums[m]});
+                        k++, m--;
+                    } else{
+                        sum > val ? m-- : k++;
                     }
                 }
             }
-            return ans;
         }
+
+        return ans;
+    }
 };
