8603e4aa1897816e32d1a7a7ab002b52ab9c0cd0
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-04-28 08:18:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (96.76%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int> mp;
-        for(int i = 0; i < s.length(); i++)
-            mp[s[i]] = i;
-        vector<int> ans;
-        int i = 0;
-        while(i < s.length()){
-            char key = s[i];
-            for(int j = i; j < mp[key]; j++)
-                if(mp[s[j]] > mp[key])
-                    key = s[j];
-            ans.push_back(abs(i - mp[key]) + 1);
-            i = mp[key] + 1;
+        int n = s.length();
+        vector<int> mark(26, -1);
+
+        //last Occurance Of Idxs
+        for(int i = 0; i < n; i++){
+            mark[s[i] - 'a'] = i;
         }
+
+        int i = 0;
+        vector<int> ans;
+        while(i < n){
+            int bIdx = i, eIdx = mark[s[i] - 'a'];
+            for(int j = i; j < eIdx; j++){
+                eIdx = max(mark[s[j] - 'a'], eIdx);
+            }
+            ans.push_back(eIdx - bIdx + 1);   i = eIdx + 1;
+        }
+        
         return ans;
     }
 };
