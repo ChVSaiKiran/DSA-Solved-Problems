d1e5be2a39de0531fe1a5d17b72aa19feab2fbda
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-05-05 08:18:10 +0530

    Time: 76 ms (14.33%), Space: 94.2 MB (17.05%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,17 +1,19 @@
 class Solution {
-    int fun(vector<int>& nums, int k) {
-        int cnt = 0, n = nums.size(), j = 0, ans = 0;
-        for(int i = 0; i < n; i++){
-            cnt += (nums[i] % 2);
-            while(cnt > k){
-                cnt -= (nums[j++] % 2);
-            }
-            ans += (i - j + 1);
-        }
-        return ans;
-    }
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return fun(nums, k) - fun(nums, k - 1);
+        int cnt = 0, sum = 0, n = nums.size();
+        unordered_map<int, int> mp; mp[0] = 1;
+
+        for(int i = 0; i < n; i++){
+            sum += (nums[i] % 2);
+
+            if(mp.count(sum - k)){
+                cnt += mp[sum - k];
+            }
+
+            mp[sum]++;
+        }
+
+        return cnt;
     }
 };
