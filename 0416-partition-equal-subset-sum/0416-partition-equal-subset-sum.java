423e29dc16b10ee057f012cec23dc096d59e5d31
CHAKKA VENKATA SAI KIRAN <106020684+ChVSaiKiran@users.noreply.github.com>
2026-09-14 22:51:12 +0530

    Time: 43 ms (61.86%), Space: 47.3 MB (63.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,20 @@
 class Solution {
-    private int solve(int i, int val, int[] nums, int[][] dp){
-        if(i >= nums.length || val < 0){
-            return 0;
-        }
-
-        if(dp[i][val] != -1){
-            return dp[i][val];
-        }
-
-        if(val == 0){
-            return dp[i][val] = 1;
-        }
-
-        int notPick = solve(i + 1, val, nums, dp);
-        int pick = solve(i + 1, val - nums[i], nums, dp);
-
-        return dp[i][val] = pick + notPick > 0 ? 1 : 0;
-    }
-
     public boolean canPartition(int[] nums) {
+        int n = nums.length;
         int sum = Arrays.stream(nums).sum(), val = sum / 2;
         if(sum % 2 == 1) return false;
 
-        int[][] dp = new int[nums.length][val + 1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[] dp = new int[val + 1]; dp[0] = 1;
+
+        for(int i = n - 1; i >= 0; i--){
+            int[] curr = new int[val + 1]; curr[0] = 1;
+            for(int j = 1; j <= val; j++){
+                int notPick = dp[j], pick = (j - nums[i] >= 0) ? dp[j - nums[i]] : 0;
+                curr[j] = (pick + notPick > 0) ? 1 : 0;
+            }
+            dp = curr;
         }
 
-        return solve(0, val, nums, dp) > 0;
+        return dp[val] > 0;
     }
 }
